045ee565dc26a0f5d7f15e060b75cee2cd974361
Alexey Shaytan <alex@intbio.org>
2017-12-19 00:03:06 +0300

    Update

--- freesasa/build.sh
+++ freesasa/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#export MACOSX_DEPLOYMENT_TARGET=10.9 - helps on some OSX platforms
 ./configure --enable-python-bindings --disable-json --disable-xml CFLAGS="-fPIC -O2" 
 make
 make install
