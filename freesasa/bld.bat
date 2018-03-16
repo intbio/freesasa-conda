bc29744c66ae8572f43b4966883d28c2e59331d9
Alexey Shaytan <molsim@users.noreply.github.com>
2018-03-16 16:18:11 +0300

    Update bld.bat

--- freesasa/bld.bat
+++ freesasa/bld.bat
@@ -1,2 +1,2 @@
 
-pip install https://pypi.python.org/packages/51/fd/4938f62b4ad2f4967da8bc0646b79eb753c7dddab168bda65cc0781ae54d/freesasa-2.0.2b2-cp27-cp27m-win32.whl#md5=bb1b7bb4e7554b7a4d6a1857f521e8ff
+pip install freesasa
