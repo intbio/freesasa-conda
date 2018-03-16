c78cdfa8e29635fdef8c6e1e57c14bc56fb5dda4
Alexey Shaytan <molsim@users.noreply.github.com>
2018-03-16 15:34:29 +0300

    Update bld.bat

--- freesasa/bld.bat
+++ freesasa/bld.bat
@@ -1,10 +1,2 @@
 
-if "%ARCH%"=="32" (
-    set TARGET="Release|Win32"
-    set BUILD_PREFIX=apr\Release
-) else (
-    set TARGET="Release|x64"
-    set BUILD_PREFIX=apr\x64\Release
-)
-
-REM in development
+pip install https://pypi.python.org/packages/de/32/90bafc5b410c4737499d469e88219ff35aee08e1d1694db4c2ade8660d5c/freesasa-2.0.2b2-cp27-cp27m-win_amd64.whl#md5=ca39196211a9cc10f959ea18c315fa7b
